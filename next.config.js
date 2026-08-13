732f4043ce23c7f072488b4dfc7c3d8e2deaca04
s0mfff <alkinegor@mail.ru>
2026-08-14 01:03:44 +0400

    setup github

--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+const repositoryName = 'dentalas';
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
+  ...(isGithubPages
+    ? {
+        output: 'export',
+        trailingSlash: true,
+        basePath: `/${repositoryName}`,
+        assetPrefix: `/${repositoryName}/`,
+      }
+    : {}),
 };
 
 module.exports = nextConfig;
